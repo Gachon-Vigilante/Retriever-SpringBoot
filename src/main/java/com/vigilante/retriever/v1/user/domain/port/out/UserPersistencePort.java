8aa26c51a65e484580360c4cac922e219d78486a
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-05 21:55:33 +0900

    Feat: 사용자 영속성 외부 port 인터페이스 추가

---
+++ src/main/java/com/vigilante/retriever/v1/user/domain/port/out/UserPersistencePort.java
@@ -0,0 +1,14 @@
+package com.vigilante.retriever.v1.user.domain.port.out;
+
+import java.util.Optional;
+
+import com.vigilante.retriever.v1.user.domain.entity.UserEntity;
+
+public interface UserPersistencePort {
+
+	Optional<UserEntity> findByLoginId(String loginId);
+
+	boolean existsByLoginId(String loginId);
+
+	UserEntity save(UserEntity userEntity);
+}
