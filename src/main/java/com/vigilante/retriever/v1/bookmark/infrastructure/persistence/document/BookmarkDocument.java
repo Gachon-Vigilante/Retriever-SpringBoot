96dfc5c6373113cda1c24466f18e91c6e52786d8
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-06 21:16:26 +0900

    Feat: 채널 북마크 mongoDB 문서 생성

---
+++ src/main/java/com/vigilante/retriever/v1/bookmark/infrastructure/persistence/document/BookmarkDocument.java
@@ -0,0 +1,31 @@
+package com.vigilante.retriever.v1.bookmark.infrastructure.persistence.document;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@Document(collection = "bookmarks")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BookmarkDocument {
+
+	@Id
+	private String id;
+
+	private String channelId;
+
+	@Field("userId")
+	private String telegramUserId;
+
+	private LocalDateTime createdAt;
+}
