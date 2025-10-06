9062f1d85a260539989a379dd96191cbc15392e8
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 01:49:17 +0900

    Feat: Ai 채널 리포트 Mapper 인터페이스 추가

---
+++ src/main/java/com/vigilante/retriever/v1/report/infrastructure/mapper/ReportPersistenceMapper.java
@@ -0,0 +1,32 @@
+package com.vigilante.retriever.v1.report.infrastructure.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.context.annotation.Primary;
+
+import com.vigilante.retriever.global.common.mapper.GenericMapper;
+import com.vigilante.retriever.v1.report.domain.entity.ReportEntity;
+import com.vigilante.retriever.v1.report.infrastructure.persistence.document.ReportDocument;
+
+@Primary
+@Mapper(
+	componentModel = "spring",
+	unmappedTargetPolicy = ReportingPolicy.IGNORE,
+	unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
+public interface ReportPersistenceMapper extends GenericMapper<ReportDocument, ReportEntity> {
+
+	@Override
+	ReportDocument toDocument(ReportEntity entity);
+
+	@Override
+	ReportEntity toEntity(ReportDocument document);
+
+	@Override
+	List<ReportDocument> getDocumentList(List<ReportEntity> entityList);
+
+	@Override
+	List<ReportEntity> getEntityList(List<ReportDocument> dodumentList);
+}
