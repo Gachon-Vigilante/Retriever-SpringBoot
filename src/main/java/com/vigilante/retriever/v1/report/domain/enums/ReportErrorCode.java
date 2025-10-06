e655b085f9a6096671d8dbad7ddddf811a655dd6
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 01:49:40 +0900

    Feat: Ai 채널 리포트 예외 처리 코드 추가

---
+++ src/main/java/com/vigilante/retriever/v1/report/domain/enums/ReportErrorCode.java
@@ -0,0 +1,15 @@
+package com.vigilante.retriever.v1.report.domain.enums;
+
+import com.vigilante.retriever.global.common.code.BaseCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ReportErrorCode implements BaseCode {
+	REPORT_NOT_FOUND("REPORT-4041", "해당하는 Ai 채널 리포트를 찾을 수 없습니다.");
+
+	private final String code;
+	private final String message;
+}
