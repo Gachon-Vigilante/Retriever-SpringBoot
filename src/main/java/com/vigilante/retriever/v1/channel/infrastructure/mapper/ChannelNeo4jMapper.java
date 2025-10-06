84d053806d62c5e69422f930f0ab221de1003017
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 03:26:10 +0900

    Feat: 채널 Neo4j Mapper 인터페이스 추가

---
+++ src/main/java/com/vigilante/retriever/v1/channel/infrastructure/mapper/ChannelNeo4jMapper.java
@@ -0,0 +1,32 @@
+package com.vigilante.retriever.v1.channel.infrastructure.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.context.annotation.Primary;
+
+import com.vigilante.retriever.global.common.mapper.GenericNeo4jMapper;
+import com.vigilante.retriever.v1.channel.domain.graphview.ChannelGraphView;
+import com.vigilante.retriever.v1.channel.infrastructure.persistence.neo4j.node.ChannelNode;
+
+@Primary
+@Mapper(
+	componentModel = "spring",
+	unmappedTargetPolicy = ReportingPolicy.IGNORE,
+	unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
+public interface ChannelNeo4jMapper extends GenericNeo4jMapper<ChannelNode, ChannelGraphView> {
+
+	@Override
+	ChannelNode toNode(ChannelGraphView graphView);
+
+	@Override
+	ChannelGraphView toGraphView(ChannelNode document);
+
+	@Override
+	List<ChannelNode> getNodeList(List<ChannelGraphView> graphViewList);
+
+	@Override
+	List<ChannelGraphView> getGraphViewList(List<ChannelNode> dodumentList);
+}
