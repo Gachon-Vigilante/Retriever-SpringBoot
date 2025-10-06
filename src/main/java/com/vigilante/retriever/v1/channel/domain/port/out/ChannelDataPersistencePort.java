f1e132eab79d856d217472ee6643be4d1928f6d0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 00:25:28 +0900

    Feat: 채널 데이터 영속성 외부 port 인터페이스 추가

---
+++ src/main/java/com/vigilante/retriever/v1/channel/domain/port/out/ChannelDataPersistencePort.java
@@ -0,0 +1,16 @@
+package com.vigilante.retriever.v1.channel.domain.port.out;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.vigilante.retriever.v1.channel.domain.entity.ChannelDataEntity;
+
+public interface ChannelDataPersistencePort {
+
+	List<ChannelDataEntity> findAll();
+
+	Optional<ChannelDataEntity> findById(String id);
+
+	// 채널 아이디로 조회
+	List<ChannelDataEntity> findByChannelId(long channelId);
+}
