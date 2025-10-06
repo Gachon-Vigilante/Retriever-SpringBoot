3647ef1a7dcefc09bc9cd006caddcd90b0d324e9
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-06 23:18:49 +0900

    Feat: 채널 북마크 command 서비스 구현

---
+++ src/main/java/com/vigilante/retriever/v1/bookmark/application/command/BookmarkCommand.java
@@ -0,0 +1,22 @@
+package com.vigilante.retriever.v1.bookmark.application.command;
+
+import com.vigilante.retriever.global.common.annotation.CommandService;
+import com.vigilante.retriever.v1.bookmark.domain.entity.BookmarkEntity;
+import com.vigilante.retriever.v1.bookmark.domain.port.out.BookmarkPersistencePort;
+
+import lombok.RequiredArgsConstructor;
+
+@CommandService
+@RequiredArgsConstructor
+public class BookmarkCommand {
+
+	private final BookmarkPersistencePort bookmarkPersistencePort;
+
+	public BookmarkEntity save(BookmarkEntity bookmarkEntity) {
+		return bookmarkPersistencePort.save(bookmarkEntity);
+	}
+
+	public void deleteById(String id) {
+		bookmarkPersistencePort.deleteById(id);
+	}
+}
