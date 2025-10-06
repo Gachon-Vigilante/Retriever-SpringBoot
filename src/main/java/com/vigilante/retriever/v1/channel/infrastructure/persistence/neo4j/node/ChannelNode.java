3b01ca377d3eace730fba0083e4e397edc87d97c
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 03:25:10 +0900

    Feat: 채널 Neo4j Node 생성

---
+++ src/main/java/com/vigilante/retriever/v1/channel/infrastructure/persistence/neo4j/node/ChannelNode.java
@@ -0,0 +1,39 @@
+package com.vigilante.retriever.v1.channel.infrastructure.persistence.neo4j.node;
+
+import java.util.Set;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.vigilante.retriever.v1.argot.infrastructure.persistence.neo4j.node.ArgotNode;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@Node("Channel")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChannelNode {
+
+	@Id
+	private Long id;
+
+	private String title;
+
+	private String username;
+
+	private String status;
+
+	private int promotedCount;
+
+	@Relationship(type = "SELLS", direction = Relationship.Direction.OUTGOING)
+	@JsonIgnoreProperties("channels")
+	private Set<ArgotNode> sellsArgots;
+}
