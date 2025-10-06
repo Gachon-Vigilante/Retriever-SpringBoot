518e9dcb85366958d40cbe72552f975d90adf13d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 00:24:03 +0900

    Feat: 채널 유사도 mongoDB 문서 생성

---
+++ src/main/java/com/vigilante/retriever/v1/channel/infrastructure/persistence/document/ChannelSimilarityDocument.java
@@ -0,0 +1,43 @@
+package com.vigilante.retriever.v1.channel.infrastructure.persistence.document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@Document(collection = "channel_similarity")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChannelSimilarityDocument {
+
+	@Id
+	private String id;
+
+	private long channelId;
+
+	private List<SimilarChannel> similarChannels;
+
+	@Field("timestamp")
+	private LocalDateTime updatedAt;
+
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
+	public static class SimilarChannel {
+
+		@Field("channelId")
+		private long Id;
+
+		private Double similarity;
+	}
+}
