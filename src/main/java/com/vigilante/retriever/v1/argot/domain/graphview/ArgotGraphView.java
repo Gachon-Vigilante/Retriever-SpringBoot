6ffeb5f39a72835b645657d0684cbf28666afa59
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-07 02:57:44 +0900

    Feat: 마약 은어 graph view 레코드 생성

---
+++ src/main/java/com/vigilante/retriever/v1/argot/domain/graphview/ArgotGraphView.java
@@ -0,0 +1,13 @@
+package com.vigilante.retriever.v1.argot.domain.graphview;
+
+import java.util.Set;
+
+import com.vigilante.retriever.v1.drug.domain.graphview.DrugGraphView;
+
+public record ArgotGraphView(
+	Long identity,
+	String name,
+	String drugId,
+	Set<DrugGraphView> refersDrugs
+) {
+}
