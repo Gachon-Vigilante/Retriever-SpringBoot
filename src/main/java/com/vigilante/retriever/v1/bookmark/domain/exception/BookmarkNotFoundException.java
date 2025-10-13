746bf20a8010cfdefd12cb94fc36f439b72f9127
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-14 00:26:15 +0900

    Fix: 오탈자 수정

--- src/main/java/com/vigilante/retriever/v1/bookmark/domain/exception/BookmarkNotFoundException.java
+++ src/main/java/com/vigilante/retriever/v1/bookmark/domain/exception/BookmarkNotFoundException.java
@@ -1,10 +1,12 @@
 package com.vigilante.retriever.v1.bookmark.domain.exception;
 
+import static com.vigilante.retriever.v1.bookmark.domain.code.BookmarkErrorCode.*;
+
 import com.vigilante.retriever.common.domain.exception.NotFoundException;
 
 public class BookmarkNotFoundException extends NotFoundException {
 
 	public BookmarkNotFoundException() {
-		super(ARGOT_NOT_FOUND);
+		super(BOOKMARK_NOT_FOUND);
 	}
 }
