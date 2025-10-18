b6086aefc1341c6d24671a585d50c1bcfcdc32d0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-18 17:25:36 +0900

    Feat: 북마크 예외처리 코드 추가

--- src/main/java/com/vigilante/retriever/v1/bookmark/domain/code/BookmarkErrorCode.java
+++ src/main/java/com/vigilante/retriever/v1/bookmark/domain/code/BookmarkErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BookmarkErrorCode implements BaseCode {
-	BOOKMARK_NOT_FOUND("BOOKMARK-4041", "해당하는 채널 북마크를 찾을 수 없습니다.");
+	BOOKMARK_NOT_FOUND("BOOKMARK-4041", "해당하는 채널 북마크를 찾을 수 없습니다."),
+	CANNOT_DELETE_OTHERS_BOOKMARK("BOOKMARK-4031", "자신의 북마크만 삭제할 수 있습니다."),
+	BOOKMARK_ALREADY_EXISTS("BOOKMARK-4091", "이미 추가된 북마크입니다.");
 
 	private final String code;
 	private final String message;
