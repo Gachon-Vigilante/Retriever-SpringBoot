4a35a2319bd5e71198ca7c22ff0df5ffaf70204d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 16:59:39 +0900

    Refactor: 예외 클래스 미사용으로 인한 로직 수정

--- src/main/java/com/vigilante/retriever/v1/channel/application/query/MessageMongoQuery.java
+++ src/main/java/com/vigilante/retriever/v1/channel/application/query/MessageMongoQuery.java
@@ -1,10 +1,12 @@
 package com.vigilante.retriever.v1.channel.application.query;
 
+import static com.vigilante.retriever.v1.channel.domain.code.MessageErrorCode.*;
+
 import java.util.List;
 
 import com.vigilante.retriever.common.domain.annotation.QueryService;
+import com.vigilante.retriever.common.domain.exception.NotFoundException;
 import com.vigilante.retriever.v1.channel.domain.entity.MessageEntity;
-import com.vigilante.retriever.v1.channel.domain.exception.MessageNotFoundException;
 import com.vigilante.retriever.v1.channel.domain.port.out.MessageMongoPort;
 
 import lombok.RequiredArgsConstructor;
@@ -20,8 +22,7 @@ public class MessageMongoQuery {
 	}
 
 	public MessageEntity getById(String id) {
-		return messageMongoPort.findById(id)
-			.orElseThrow(MessageNotFoundException::new);
+		return messageMongoPort.findById(id).orElseThrow(() -> new NotFoundException(MESSAGE_NOT_FOUND));
 	}
 
 	public List<MessageEntity> findByChannelId(long channelId) {
