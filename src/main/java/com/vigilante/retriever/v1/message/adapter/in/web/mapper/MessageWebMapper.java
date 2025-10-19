04d0f1ef4818081554185570f5d4572c88e363df
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 17:17:54 +0900

    Feat: 메시지 서비스 로직 DTO와 컨트롤러 DTO의 Mapper 생성

---
+++ src/main/java/com/vigilante/retriever/v1/message/adapter/in/web/mapper/MessageWebMapper.java
@@ -0,0 +1,54 @@
+package com.vigilante.retriever.v1.message.adapter.in.web.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.vigilante.retriever.v1.message.adapter.in.web.dto.response.MessageInfoResponse;
+import com.vigilante.retriever.v1.message.domain.entity.MessageEntity;
+
+@Component
+public class MessageWebMapper {
+
+	public MessageInfoResponse toResponse(MessageEntity entity) {
+		return MessageInfoResponse.builder()
+			.id(entity.id())
+			.channelId(entity.channelId())
+			.message(entity.message())
+			.messageId(entity.messageId())
+			.date(entity.date())
+			.editDate(entity.editDate())
+			.editHide(entity.editHide())
+			.entities(entity.entities())
+			.forwards(entity.forwards())
+			.fromId(entity.fromId())
+			.fwdDate(entity.fwdDate())
+			.fwdFromId(entity.fwdFromId())
+			.fwdFromName(entity.fwdFromName())
+			.groupedId(entity.groupedId())
+			.legacy(entity.legacy())
+			.media(MessageInfoResponse.Media.builder()
+				.url(entity.media().url())
+				.type(entity.media().type())
+				.build())
+			.mediaUnread(entity.mediaUnread())
+			.mentioned(entity.mentioned())
+			.out(entity.out())
+			.post(entity.post())
+			.reactions(entity.reactions())
+			.replyToMsgId(entity.replyToMsgId())
+			.senderType(entity.senderType())
+			.silent(entity.silent())
+			.updatedAt(entity.updatedAt())
+			.viaBotId(entity.viaBotId())
+			.views(entity.views())
+			.argots(entity.argots())
+			.build();
+	}
+
+	public List<MessageInfoResponse> toResponseList(List<MessageEntity> entities) {
+		return entities.stream()
+			.map(this::toResponse)
+			.toList();
+	}
+}
