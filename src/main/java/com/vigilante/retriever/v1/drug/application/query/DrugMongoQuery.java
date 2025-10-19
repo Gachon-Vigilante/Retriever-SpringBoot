0a11fb9c3689c01d2c12249aa9a1b8aa4f6ff45e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 13:00:30 +0900

    Refactor: 예외 클래스 미사용으로 인한 로직 수정

--- src/main/java/com/vigilante/retriever/v1/drug/application/query/DrugMongoQuery.java
+++ src/main/java/com/vigilante/retriever/v1/drug/application/query/DrugMongoQuery.java
@@ -1,10 +1,12 @@
 package com.vigilante.retriever.v1.drug.application.query;
 
+import static com.vigilante.retriever.v1.drug.domain.code.DrugErrorCode.*;
+
 import java.util.List;
 
 import com.vigilante.retriever.common.domain.annotation.QueryService;
+import com.vigilante.retriever.common.domain.exception.NotFoundException;
 import com.vigilante.retriever.v1.drug.domain.entity.DrugEntity;
-import com.vigilante.retriever.v1.drug.domain.exception.DrugNotFoundException;
 import com.vigilante.retriever.v1.drug.domain.port.out.DrugMongoPort;
 
 import lombok.RequiredArgsConstructor;
@@ -20,10 +22,10 @@ public class DrugMongoQuery {
 	}
 
 	public DrugEntity getById(String id) {
-		return drugMongoPort.findById(id).orElseThrow(DrugNotFoundException::new);
+		return drugMongoPort.findById(id).orElseThrow(() -> new NotFoundException(DRUG_NOT_FOUND));
 	}
 
-	List<DrugEntity> getByArgot(String argot) {
+	public List<DrugEntity> getByArgot(String argot) {
 		return drugMongoPort.findByArgot(argot);
 	}
 }
