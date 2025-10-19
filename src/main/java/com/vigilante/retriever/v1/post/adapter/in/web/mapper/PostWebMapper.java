dc1ff4acd3948be4e6936b6f1c97a5cdbee86cb0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 21:11:50 +0900

    Feat: 게시글 그래프 서비스 로직 DTO와 컨트롤러 DTO의 Mapper 생성

--- src/main/java/com/vigilante/retriever/v1/post/adapter/in/web/mapper/PostWebMapper.java
+++ src/main/java/com/vigilante/retriever/v1/post/adapter/in/web/mapper/PostWebMapper.java
@@ -1,16 +1,28 @@
 package com.vigilante.retriever.v1.post.adapter.in.web.mapper;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import com.vigilante.retriever.v1.channel.adapter.in.web.mapper.ChannelWebMapper;
+import com.vigilante.retriever.v1.post.adapter.in.web.dto.request.CreatePromotionRelationRequest;
+import com.vigilante.retriever.v1.post.adapter.in.web.dto.response.PostGraphInfoResponse;
 import com.vigilante.retriever.v1.post.adapter.in.web.dto.response.PostInfoResponse;
 import com.vigilante.retriever.v1.post.adapter.in.web.dto.response.PostPageResponse;
+import com.vigilante.retriever.v1.post.domain.dto.command.CreatePromotionRelationCommand;
 import com.vigilante.retriever.v1.post.domain.entity.PostEntity;
+import com.vigilante.retriever.v1.post.domain.graphview.PostGraphView;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class PostWebMapper {
+
+	private final ChannelWebMapper channelWebMapper;
 
 	public PostInfoResponse toResponse(PostEntity entity) {
 		return PostInfoResponse.builder()
@@ -97,5 +109,49 @@ public class PostWebMapper {
 			.totalCount(postInfoPage.getTotalElements())
 			.posts(postInfoPage.getContent())
 			.build();
+	}
+
+	public CreatePromotionRelationCommand toCommand(CreatePromotionRelationRequest createPromotionRelationRequest) {
+		return CreatePromotionRelationCommand.builder()
+			.id(createPromotionRelationRequest.id())
+			.postId(createPromotionRelationRequest.postId())
+			.build();
+	}
+
+	public PostGraphInfoResponse toGraphResponse(PostGraphView graphView) {
+		return PostGraphInfoResponse.builder()
+			.postId(graphView.postId())
+			.cluster(graphView.cluster())
+			.link(graphView.link())
+			.content(graphView.content())
+			.title(graphView.title())
+			.domain(graphView.domain())
+			.siteName(graphView.siteName())
+			.createdAt(graphView.createdAt())
+			.updatedAt(graphView.updatedAt())
+			.promotesChannels(mapPromotesChannels(graphView.promotesChannels()))
+			.similarPosts(graphView.similarPosts().stream()
+				.map(this::toGraphResponse)
+				.collect(Collectors.toSet()))
+			.build();
+	}
+
+	private Set<PostGraphInfoResponse.Promote> mapPromotesChannels(Set<PostGraphView.Promote> promotesChannels) {
+		if (promotesChannels == null) {
+			return null;
+		}
+
+		return promotesChannels.stream()
+			.map(promote -> PostGraphInfoResponse.Promote.builder()
+				.id(promote.id())
+				.channel(channelWebMapper.toGraphResponse(promote.channel()))
+				.build())
+			.collect(Collectors.toSet());
+	}
+
+	public List<PostGraphInfoResponse> toGraphResponseList(List<PostGraphView> graphViews) {
+		return graphViews.stream()
+			.map(this::toGraphResponse)
+			.toList();
 	}
 }
