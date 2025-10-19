35fc521f5629556798e8914e6d40a8622d7aabad
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 21:23:00 +0900

    Feat: 게시글 그래프 예외 처리 코드 추가

--- src/main/java/com/vigilante/retriever/v1/post/domain/code/PostErrorCode.java
+++ src/main/java/com/vigilante/retriever/v1/post/domain/code/PostErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PostErrorCode implements BaseCode {
-	POST_NOT_FOUND("POST-4041", "해당하는 게시글을 찾을 수 없습니다.");
+
+	FAIL_UPDATE_POST_ID("POST-4001", "게시글 ID 업데이트에 실패했습니다."),
+	POST_NOT_FOUND("POST-4041", "해당하는 게시글을 찾을 수 없습니다."),
+	ALREADY_EXIST_PROMOTE_RELATION("POST-4091", "이미 홍보 관계가 존재합니다.");
 
 	private final String code;
 	private final String message;
