e7d99b57227a2d07a4d025fd236561f80aba11f2
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 18:34:26 +0900

    Refactor: MongoDB 메서드 정리
    - 재설계 메서드 미사용 판단으로 삭제 처리
    - 전체 게시글 조회 페이지네이션 추가
    - 예외 클래스 미사용

--- src/main/java/com/vigilante/retriever/v1/post/application/query/PostMongoQuery.java
+++ src/main/java/com/vigilante/retriever/v1/post/application/query/PostMongoQuery.java
@@ -1,10 +1,15 @@
 package com.vigilante.retriever.v1.post.application.query;
 
+import static com.vigilante.retriever.v1.post.domain.code.PostErrorCode.*;
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.vigilante.retriever.common.domain.annotation.QueryService;
+import com.vigilante.retriever.common.domain.exception.NotFoundException;
 import com.vigilante.retriever.v1.post.domain.entity.PostEntity;
-import com.vigilante.retriever.v1.post.domain.exception.PostNotFoundException;
 import com.vigilante.retriever.v1.post.domain.port.out.PostMongoPort;
 
 import lombok.RequiredArgsConstructor;
@@ -15,27 +20,17 @@ public class PostMongoQuery {
 
 	private final PostMongoPort postMongoPort;
 
-	public List<PostEntity> findAll() {
-		return postMongoPort.findAll();
+	public Page<PostEntity> findAll(Pageable pageable) {
+		return postMongoPort.findAll(pageable);
 	}
 
 	public PostEntity getById(String id) {
-		return postMongoPort.findById(id)
-			.orElseThrow(PostNotFoundException::new);
+		return postMongoPort.findById(id).orElseThrow(() -> new NotFoundException(POST_NOT_FOUND));
 	}
 
 	public List<PostEntity> findByTitleContaining(String title) {
 		return postMongoPort.findByTitleContaining(title);
 	}
-
-	// TODO: 로직 재설계 필요
-	// public List<PostEntity> findByPromoChannelId(String promoChannelId) {
-	// 	return postMongoPort.findByPromoChannelId(promoChannelId);
-	// }
-	//
-	// public List<PostEntity> findByAuthor(String author) {
-	// 	return postMongoPort.findByAuthor(author);
-	// }
 
 	public List<PostEntity> findByLinkOrderByCreatedAtAsc(String link) {
 		return postMongoPort.findByLinkOrderByDiscoveredAtAsc(link);
--- src/main/java/com/vigilante/retriever/v1/post/domain/port/out/PostMongoPort.java
+++ src/main/java/com/vigilante/retriever/v1/post/domain/port/out/PostMongoPort.java
@@ -3,22 +3,20 @@ package com.vigilante.retriever.v1.post.domain.port.out;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.vigilante.retriever.v1.post.domain.entity.PostEntity;
 
 public interface PostMongoPort {
 
 	PostEntity save(PostEntity post);
 
-	List<PostEntity> findAll();
+	Page<PostEntity> findAll(Pageable pageable);
 
 	Optional<PostEntity> findById(String id);
 
 	List<PostEntity> findByTitleContaining(String title);
-
-	// TODO: 로직 재설계 필요
-	// List<PostEntity> findByPromoChannelId(String promoChannelId);
-	//
-	// List<PostEntity> findByAuthor(String author);
 
 	List<PostEntity> findByLinkOrderByDiscoveredAtAsc(String link);
 }
