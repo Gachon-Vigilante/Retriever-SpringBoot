1af18d3b296ab6a29b978571ea632842472cf181
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 14:38:28 +0900

    Feat: 마약 은어 서비스 로직 구현

---
+++ src/main/java/com/vigilante/retriever/v1/argot/application/service/GetArgotGraphService.java
@@ -0,0 +1,28 @@
+package com.vigilante.retriever.v1.argot.application.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.vigilante.retriever.v1.argot.application.query.ArgotNeo4jQuery;
+import com.vigilante.retriever.v1.argot.domain.graphview.ArgotGraphView;
+import com.vigilante.retriever.v1.argot.domain.port.in.GetArgotGraphUseCase;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GetArgotGraphService implements GetArgotGraphUseCase {
+
+	private final ArgotNeo4jQuery argotNeo4jQuery;
+
+	@Override
+	public List<ArgotGraphView> findAll() {
+		return argotNeo4jQuery.findAll();
+	}
+
+	@Override
+	public List<ArgotGraphView> findAllWithRefersTo() {
+		return argotNeo4jQuery.findAllWithRefersTo();
+	}
+}
