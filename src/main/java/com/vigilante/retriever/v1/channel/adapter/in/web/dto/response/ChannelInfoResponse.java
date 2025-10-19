76fc801352531d6e66b2880f6a9e870fccaaaeb2
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-19 15:54:48 +0900

    Feat: 채널 컨트롤러 계층 응답 DTO 추가

---
+++ src/main/java/com/vigilante/retriever/v1/channel/adapter/in/web/dto/response/ChannelInfoResponse.java
@@ -0,0 +1,51 @@
+package com.vigilante.retriever.v1.channel.adapter.in.web.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.vigilante.retriever.v1.channel.domain.enums.ChannelStatus;
+
+import lombok.Builder;
+
+@Builder
+public record ChannelInfoResponse(
+	String id,
+	String title,
+	String username,
+	Long channelId,
+	String about,
+	Long accessHash,
+	Boolean broadcast,
+	Boolean callActive,
+	Boolean callNotEmpty,
+	LocalDateTime checkedAt,
+	LocalDateTime date,
+	Boolean fake,
+	Boolean gigaGroup,
+	Boolean hasGeo,
+	Boolean hasLink,
+	LocalDateTime lastMessageDate,
+	Boolean left,
+	Boolean megaGroup,
+	Boolean min,
+	Boolean monitoring,
+	Boolean noForwards,
+	Integer participantsCount,
+	String photo,
+	Boolean restricted,
+	List<RestrictionReason> restrictionReason,
+	Boolean scam,
+	Boolean signatures,
+	Boolean slowModeEnabled,
+	ChannelStatus status,
+	LocalDateTime updatedAt,
+	Boolean verified
+) {
+	@Builder
+	public record RestrictionReason(
+		String platform,
+		String reason,
+		String text
+	) {
+	}
+}
