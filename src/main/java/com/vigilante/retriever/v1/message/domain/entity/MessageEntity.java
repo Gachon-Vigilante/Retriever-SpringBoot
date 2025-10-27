657a2a4f3a28224aa30f4cce7648130e8b247fe5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-27 22:41:09 +0900

    Refactor: 메시지 엔티티 수정

--- src/main/java/com/vigilante/retriever/v1/message/domain/entity/MessageEntity.java
+++ src/main/java/com/vigilante/retriever/v1/message/domain/entity/MessageEntity.java
@@ -41,7 +41,11 @@ public record MessageEntity(
 	@Builder
 	public record Media(
 		String url,
-		String type
+		String fileType,
+		String mineType,
+		String fileId,
+		String accessHash,
+		Long fileSize
 	) {
 	}
 }
