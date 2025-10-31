91b9ea2c6130b2a5ae4ce60a1b0683947b8eef01
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-01 01:32:15 +0900

    Feat: 채널 노드 상세 정보 조회 Neo4j 메서드 추가

--- src/main/java/com/vigilante/retriever/v1/channel/adapter/out/persistence/neo4j/adapter/ChannelNeo4jQueryAdapter.java
+++ src/main/java/com/vigilante/retriever/v1/channel/adapter/out/persistence/neo4j/adapter/ChannelNeo4jQueryAdapter.java
@@ -38,6 +38,11 @@ public class ChannelNeo4jQueryAdapter implements ChannelNeo4jPort {
 	}
 
 	@Override
+	public Optional<ChannelGraphView> findChannelWithAllRelations(Long channelId) {
+		return channelNeo4jRepository.findChannelWithAllRelations(channelId).map(channelNeo4jMapper::toGraphView);
+	}
+
+	@Override
 	public ChannelGraphView save(ChannelGraphView channelGraphView) {
 		ChannelNode toSave = channelNeo4jMapper.toNode(channelGraphView);
 		ChannelNode saved = channelNeo4jRepository.save(toSave);
