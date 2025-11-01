ff592d933a08d2d3807133c5234eb5731dbc29e3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-01 13:37:03 +0900

    Feat: 마약 노드 상세 정보 조회 API 예제 키 상수 정의

--- src/main/java/com/vigilante/retriever/adapter/web/openapi/constant/ExampleKeyConstant.java
+++ src/main/java/com/vigilante/retriever/adapter/web/openapi/constant/ExampleKeyConstant.java
@@ -46,6 +46,8 @@ public final class ExampleKeyConstant {
 	public static final String DRUG_GET_BY_ARGOT_200 = "Drug__Get__By__Argot__200";
 	public static final String DRUG_GET_BY_ID_404 = "Drug__Get__By__Id__404";
 	public static final String DRUG_GRAPH_FIND_ALL_200 = "Drug__Graph__Find__All__200";
+	public static final String DRUG_GET_TRACE_200 = "Drug__Get__Trace__200";
+	public static final String DRUG_GET_TRACE_404 = "Drug__Get__Trace__404";
 
 	// Argot
 	public static final String ARGOT_FIND_ALL_200 = "Argot__Find__All__200";
