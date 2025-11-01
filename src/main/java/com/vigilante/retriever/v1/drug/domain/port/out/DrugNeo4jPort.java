549fde34780f4898b28ce1a9038a2fcea9ea13f1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-01 13:33:20 +0900

    Feat: 마약 노드 상세 정보 조회 Neo4j 메서드 추가

--- src/main/java/com/vigilante/retriever/v1/drug/domain/port/out/DrugNeo4jPort.java
+++ src/main/java/com/vigilante/retriever/v1/drug/domain/port/out/DrugNeo4jPort.java
@@ -1,10 +1,15 @@
 package com.vigilante.retriever.v1.drug.domain.port.out;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.query.Param;
 
 import com.vigilante.retriever.v1.drug.domain.graphview.DrugGraphView;
 
 public interface DrugNeo4jPort {
 
 	List<DrugGraphView> findAll();
+
+	Optional<DrugGraphView> findDrugWithAllRelationships(@Param("drugBankId") String drugBankId);
 }
